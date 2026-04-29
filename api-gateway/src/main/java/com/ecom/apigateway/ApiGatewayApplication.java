9687d4355ee9bc9c957a4cc168d47222627041f1
Gopalsamy26 <gopal@ausmit.in>
2026-04-29 05:39:41 +0530

    service-registry and api gateway  changes

--- api-gateway/src/main/java/com/ecom/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/ecom/apigateway/ApiGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.ecom.userservice;
+package com.ecom.apigateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,10 +6,8 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class UserserviceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(UserserviceApplication.class, args);
-	}
-
+public class ApiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
